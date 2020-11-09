8f743d9ef8b5c3ac9fb68615f085671e482dc57a
steventimberman <stevetimberman@gmail.com>
2020-11-08 17:29:39 -0800

    Updates

---
+++ precache-manifest.cc3e4580fe2f39050bb104e7a32cd44b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43aa04ac5e97411b3e460cc4cc2a350e",
+    "url": "/applets/index.html"
+  },
+  {
+    "revision": "dcf640ea16d376f4c1d3",
+    "url": "/applets/static/js/2.a7cb71fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/applets/static/js/2.a7cb71fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4183df1f3667af195bdf",
+    "url": "/applets/static/js/main.1ed711cc.chunk.js"
+  },
+  {
+    "revision": "4c15e4625e6b49f8b58c",
+    "url": "/applets/static/js/runtime-main.b347368f.js"
+  }
+]);
